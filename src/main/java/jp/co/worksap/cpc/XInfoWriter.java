d74ae541d2a4d1674413a75977360be2ee359431
kotaro sasaki <sasaki@worksap.co.jp>
2016-02-24 16:17:30 +0900

    Mock Sample Data Add

--- src/main/java/jp/co/worksap/cpc/XInfoWriter.java
+++ src/main/java/jp/co/worksap/cpc/XInfoWriter.java
@@ -13,7 +13,6 @@ import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XInfoWriter {
@@ -34,6 +33,7 @@ public class XInfoWriter {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		new XInfoWriter().execute();
+		new XInfoWriter().createExcel();
 	}
 
 
@@ -46,36 +46,187 @@ public class XInfoWriter {
 		//データを設定
 		//Excelを出力
 
+		//System.getProperties().list(System.out);
+
+		String app = System.getProperty("sun.java.command", "XInfoWiter");
+		System.out.println(app);
+		System.out.println(FILEPATH);
+
+//		XInfoMock xinfomock = new XInfoMock();
+//		System.out.println(xinfomock.fixedLine[0].length);
+
 	}
 
 	private void createExcel() {
 
-		//System.out.println(CURRENT_DIR);
-		//String filePath = new File(CURRENT_DIR, BOOK_NAME).getPath();
-		System.out.println(FILEPATH);
-		//System.exit(0);
-
+		//Excel work
 		try (Workbook book = new XSSFWorkbook(); ) {
 
+			//使用するfont関連を定義(黒・赤・青の３種 黒太字＋１種)
+			Font font = book.createFont();
+			font.setFontName("Arial");
+			font.setFontHeightInPoints((short) 10);
+			font.setColor(IndexedColors.BLACK.getIndex());
+
+			Font font_blue = book.createFont();
+			font_blue.setFontName("Arial");
+			font_blue.setFontHeightInPoints((short) 10);
+			font_blue.setColor(IndexedColors.BLUE.getIndex());
+
+			Font font_red = book.createFont();
+			font_red.setFontName("Arial");
+			font_red.setFontHeightInPoints((short) 10);
+			font_red.setColor(IndexedColors.RED.getIndex());
+
+			Font font_bold = book.createFont();
+			font_bold.setFontName("Arial");
+			font_bold.setFontHeightInPoints((short) 10);
+			font_bold.setColor(IndexedColors.BLACK.getIndex());
+			font_bold.setBold(true);
+
+			//Cellの種類別 Style定義
+			/* normal (文字/背景) (黒/白) (黒/灰) (黒/青) (黒/水) (赤/白) (赤/灰) (青/白) (Bold/白)
+			 * 書式 数字 文字 日付 (折り返し表示)
+			 */
 			//style & font set
-			CellStyle style = book.createCellStyle();
+			CellStyle styleHead = book.createCellStyle();
 
 			//set border
-			style.setBorderTop(CellStyle.BORDER_THIN);
-			style.setBorderBottom(CellStyle.BORDER_HAIR);
-			style.setBorderLeft(CellStyle.BORDER_MEDIUM);
-			style.setBorderRight(CellStyle.BORDER_DOTTED);
+			styleHead.setBorderTop(CellStyle.BORDER_THIN);
+			styleHead.setBorderBottom(CellStyle.BORDER_THIN);
+			styleHead.setBorderLeft(CellStyle.BORDER_THIN);
+			styleHead.setBorderRight(CellStyle.BORDER_THIN);
 
-			Font font = book.createFont();
-			font.setColor(IndexedColors.AQUA.getIndex());
+			styleHead.setFillPattern(CellStyle.SOLID_FOREGROUND);
+			styleHead.setFillForegroundColor(IndexedColors.PALE_BLUE.index);
+//			styleHead.setFillBackgroundColor(IndexedColors.RED.index);
+			styleHead.setFont(font);
 
 			Row  row    = null;
 			Cell cell   = null;
 			int  rowNum = 0;
 			int  colNum = 0;
+			String fixedval = "";
 
 			Sheet sheet = null;
+			sheet = book.createSheet(SHEET_NAME);
 
+			//load mock data
+			XInfoMock xinfomock = new XInfoMock();
+
+			for (int i = 0; i < xinfomock.fixedLine.length; i++) {
+
+				row = sheet.createRow(i);
+
+				for (int j = 0; j < xinfomock.fixedLine[i].length; j++) {
+
+					fixedval = xinfomock.fixedLine[i][j];
+
+					cell = row.createCell(j);
+					cell.setCellValue(fixedval);
+
+					cell.setCellStyle(styleHead);
+				}
+
+			}
+
+			//COMPANY fixed block 縦横変換している
+			CellStyle styleCpny = book.createCellStyle();
+
+			//set border
+			styleCpny.setBorderTop(CellStyle.BORDER_THIN);
+			styleCpny.setBorderBottom(CellStyle.BORDER_THIN);
+			styleCpny.setBorderLeft(CellStyle.BORDER_THIN);
+			styleCpny.setBorderRight(CellStyle.BORDER_THIN);
+
+			styleCpny.setFillPattern(CellStyle.SOLID_FOREGROUND);
+			styleCpny.setFillForegroundColor(IndexedColors.LIGHT_TURQUOISE.index);
+			styleCpny.setFont(font);
+
+			int curtCol = xinfomock.fixedLine[0].length;
+
+			for (int i = 0; i < xinfomock.cnpyFixedBlock[0].length; i++) {
+
+				row = sheet.getRow(i);
+
+				fixedval = "";
+				for (int j = 0; j < xinfomock.cnpyFixedBlock.length; j++) {
+
+					cell = row.createCell(curtCol + j);
+
+					if (!fixedval.equals(xinfomock.cnpyFixedBlock[j][i])) {
+						fixedval = xinfomock.cnpyFixedBlock[j][i];
+						cell.setCellValue(fixedval);
+					}
+
+					cell.setCellStyle(styleCpny);
+				}
+
+			}
+
+			//was fixed block
+			CellStyle styleWas = book.createCellStyle();
+
+			//set border
+			styleWas.setBorderTop(CellStyle.BORDER_THIN);
+			styleWas.setBorderBottom(CellStyle.BORDER_THIN);
+			styleWas.setBorderLeft(CellStyle.BORDER_THIN);
+			styleWas.setBorderRight(CellStyle.BORDER_THIN);
+
+			styleWas.setFont(font);
+
+			int curtRow = xinfomock.fixedLine.length;
+
+			for (int i = 0; i < xinfomock.wasFixedBlock.length; i++) {
+
+				row = sheet.createRow(curtRow + i);
+
+				fixedval = "";
+				for (int j = 0; j < xinfomock.wasFixedBlock[i].length; j++) {
+
+					fixedval = xinfomock.wasFixedBlock[i][j];
+
+					cell = row.createCell(j);
+					cell.setCellValue(fixedval);
+
+					cell.setCellStyle(styleWas);
+				}
+
+			}
+
+			//COMPANY fixed block 縦横変換している
+			CellStyle styleData = book.createCellStyle();
+
+			//set border
+			styleData.setBorderTop(CellStyle.BORDER_THIN);
+			styleData.setBorderBottom(CellStyle.BORDER_THIN);
+			styleData.setBorderLeft(CellStyle.BORDER_THIN);
+			styleData.setBorderRight(CellStyle.BORDER_THIN);
+
+			styleData.setFont(font);
+
+			curtRow = xinfomock.fixedLine.length;
+			curtCol = xinfomock.fixedLine[0].length;
+
+			for (int i = 0; i < xinfomock.wasSupportData[0].length; i++) {
+
+				row = sheet.getRow(curtRow + i);
+
+				fixedval = "";
+				for (int j = 0; j < xinfomock.wasSupportData.length; j++) {
+
+					cell = row.createCell(curtCol + j);
+
+					fixedval = xinfomock.wasSupportData[j][i];
+					cell.setCellValue(fixedval);
+
+					cell.setCellStyle(styleData);
+				}
+
+			}
+
+
+/*
 			for (int i = 0; i < 3; i++) {
 				//sheet create & name set
 				sheet = book.createSheet(SHEET_NAME+i);
@@ -84,24 +235,6 @@ public class XInfoWriter {
 				rowNum = 0;
 				colNum = 0;
 				row = sheet.createRow(rowNum);
-
-				cell = row.createCell(colNum);
-				cell.setCellValue(rowNum+"-"+colNum);  //set cell value
-
-				cell = row.createCell(++colNum);
-				cell.setCellValue(rowNum+"-"+colNum+"longlonglongcell");
-
-				cell = row.createCell(++colNum);
-				cell.setCellValue(rowNum+"-"+colNum);
-
-				cell = row.createCell(++colNum);
-				cell.setCellValue(rowNum+"-"+colNum);
-
-				cell = row.createCell(++colNum);
-				cell.setCellValue(rowNum+"-"+colNum);
-
-				cell = row.createCell(++colNum);
-				cell.setCellValue(rowNum+"-"+colNum);
 
 				//fixed
 				sheet.createFreezePane(1, 1);
@@ -130,17 +263,14 @@ public class XInfoWriter {
 					for (int j2 = 0; j2 < 6; j2++) {
 						sheet.autoSizeColumn(j2, true);
 					}
-
 				}
 				sheet.addMergedRegion(new CellRangeAddress(1, 2, 1, 3));
-
-
 			}
-
-
 
 			style.setFont(font);
 			cell.setCellStyle(style);
+*/
+
 
 			//output file
 			try (FileOutputStream out = new FileOutputStream(FILEPATH);) {
